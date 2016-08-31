e8484332bf5bd796a5cdedc91886a2f8d467b82b
Jet Wang <ajasonwang@gmail.com>
2016-08-31 10:21:03 +0800

    fix categories and tags display issue.

---
+++ _pages/about.md
@@ -0,0 +1,20 @@
+---
+layout: single
+title : 我是谁
+header : About Me
+permalink: /about.html
+---
+
+
+	1，2006年《时代周刊》年度风云人物
+
+	2，技术宅
+
+	3，大叔
+
+	4，没有然后了
+
+
+联系方式：
+
+[![](/images/weixin.png)](/images/weixin.png)
--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @echo off
 
 set SSL_CERT_FILE=C:\Ruby200\bin\cacert.pem
-::jekyll serve -w --incremental
 ::bundle exec jekyll serve --incremental
-bundle exec jekyll serve --config _config.dev.yml --incremental
+bundle exec jekyll serve --config _config.dev.yml --incremental -H 0.0.0.0 -P 5000
